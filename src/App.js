77ce422e86dd79599e3b409fc7a557b1ba6d6928
Dina Fine <dinagiacobbi@gmail.com>
2021-12-23 11:08:04 -0900

    fixed merges rebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-<<<<<<< HEAD
+
 import './App.css';
 import SearchForm from './components/SearchForm/SearchForm';
 
 
-=======
+
 import "./App.css";
 import Map from "./components/Map";
+
 import Nav from "./components/Nav/Nav";
 import GemForm from "./components/GemForm/GemForm";
->>>>>>> eabfdb4 (Map and basic components)
 
 function App() {
   return (
     <div className="App">
       <Map />
-      {/* <GemForm /> */}
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -6,6 +6,8 @@ import MapGL, {
   GeolocateControl,
   NavigationControl,
 } from "react-map-gl";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import useModal from "../hooks/useModals";
 import Geocoder from "react-map-gl-geocoder";
 import Nav from "./Nav/Nav";
@@ -17,8 +19,13 @@ import Filter from "./FilterCard/Filter";
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const Map = () => {
-  const { displayGemForm, displayGems, displayProfile, displayFilter } =
-    useModal();
+  const {
+    displayGemForm,
+    displayGems,
+    displayProfile,
+    displayFilter,
+    closeForm,
+  } = useModal();
 
   const [selected, setSelected] = useState(null);
   const [userPos, setUserPos] = useState([]);
@@ -32,49 +39,105 @@ const Map = () => {
     }
 
     navigator.geolocation.getCurrentPosition(getPos);
-
     setData([
       {
         lat: 39.6061,
         lng: -106.355,
         name: "Vail Mtn",
-        category: "rec",
+        category: "Outdoors",
         likes: 24,
+        reviews: 25,
+        description: "A ski place",
       },
       {
         lat: 43.582767,
         lng: -110.821999,
         name: "Jackson Hole",
-        category: "food",
+        category: "Food",
         likes: 50,
+        reviews: 57,
+        description: "A ski place",
       },
       {
         lat: 40.4572,
         lng: -106.8045,
         name: "Steamboat",
-        category: "education",
+        category: "Education",
         likes: 88,
+        reviews: 100,
+        description: "A ski place",
       },
       {
         lat: 39.8841,
         lng: -105.7627,
         name: "Winter Park",
-        category: "rec",
+        category: "Outdoors",
         likes: 50,
+        reviews: 70,
+        description: "A ski place",
       },
       {
         lat: 39.6425,
         lng: -105.8719,
         name: "Arapahoe Basin",
-        category: "education",
+        category: "Education",
         likes: 60,
+        reviews: 63,
+        description: "A ski place",
       },
       {
         lat: 40.5884,
         lng: -111.6386,
         name: "Alta",
-        category: "food",
+        category: "Food",
         likes: 89,
+        reviews: 100,
+        description: "A ski place",
+      },
+      {
+        lat: 48.8606,
+        lng: 2.3376,
+        name: "The Louvre",
+        category: "Education",
+        likes: 77,
+        reviews: 140,
+        description: "A Museum",
+      },
+      {
+        lat: 32.6118,
+        lng: -80.0233,
+        name: "The Ocean Course",
+        category: "Sporting",
+        likes: 99,
+        reviews: 112,
+        description: "A golf course",
+      },
+      {
+        lat: 36.5725,
+        lng: -121.9486,
+        name: "Pebble Beach",
+        category: "Sporting",
+        likes: 84,
+        reviews: 190,
+        description: "A golf course",
+      },
+      {
+        lat: 40.9673,
+        lng: -74.294,
+        name: "Chanos",
+        category: "Food",
+        likes: 70,
+        reviews: 83,
+        description: "The best burritos",
+      },
+      {
+        lat: 40.6892,
+        lng: -74.0445,
+        name: "The Statue of Libery",
+        category: "Sight Seeing",
+        likes: 82,
+        reviews: 103,
+        description: "Some Statue",
       },
     ]);
   }, []);
@@ -136,11 +199,14 @@ const Map = () => {
       {
         lat: userPos[0],
         lng: userPos[1],
-        name: "Me",
-        category: "food",
-        likes: 2,
+        name: "My home",
+        category: "Other",
+        likes: 8,
+        reviews: 8,
+        description: "I live here",
       },
     ]);
+    console.log(data);
     setViewport({
       ...viewport,
       zoom: 17,
@@ -148,6 +214,7 @@ const Map = () => {
       longitude: userPos[1],
       transitionDuration: 1000,
     });
+    closeForm();
   }
 
   const navControlStyle = {
@@ -194,7 +261,23 @@ const Map = () => {
                 offsetLeft={-10}
                 offsetTop={-10}
               >
-                {el.category === "food" ? (
+                {calcDist(el.lat, el.lng) < 0.11 ? (
+                  <i
+                    style={{ color: "#00ffcb", transform: "scale(1.3)" }}
+                    class="far fa-gem"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setSelected(el);
+                      setViewport({
+                        ...viewport,
+                        zoom: 17,
+                        latitude: el.lat,
+                        longitude: el.lng,
+                        transitionDuration: 1000,
+                      });
+                    }}
+                  ></i>
+                ) : el.category === "Food" ? (
                   <i
                     style={{ color: "red" }}
                     class="far fa-gem"
@@ -210,7 +293,7 @@ const Map = () => {
                       });
                     }}
                   ></i>
-                ) : el.category === "education" ? (
+                ) : el.category === "Education" ? (
                   <i
                     style={{ color: "blue" }}
                     class="far fa-gem"
@@ -254,16 +337,60 @@ const Map = () => {
             offsetTop={-10}
             onClose={() => setSelected(null)}
           >
-            <h3>{selected.name}</h3>
-            <h6>
-              {calcDist(selected.lat, selected.lng).toFixed(2)} miles from you
-            </h6>
+            <div
+              style={{
+                width: "250px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <div>
+                <h3>{selected.name}</h3>
+                <h6>
+                  {calcDist(selected.lat, selected.lng).toFixed(2)} miles from
+                  you
+                </h6>
+              </div>
+              <div style={{ width: "60px" }}>
+                <CircularProgressbar
+                  value={((selected.likes / selected.reviews) * 100).toFixed(0)}
+                  text={`${((selected.likes / selected.reviews) * 100).toFixed(
+                    0
+                  )}%`}
+                  strokeWidth={10}
+                  styles={buildStyles({
+                    pathColor: `${
+                      ((selected.likes / selected.reviews) * 100).toFixed(0) <
+                      60
+                        ? "#e74c3c"
+                        : ((selected.likes / selected.reviews) * 100).toFixed(
+                            0
+                          ) < 85
+                        ? "#f1c40f"
+                        : "#27ae60"
+                    }`,
+                    textColor: `${
+                      ((selected.likes / selected.reviews) * 100).toFixed(0) <
+                      60
+                        ? "#e74c3c"
+                        : ((selected.likes / selected.reviews) * 100).toFixed(
+                            0
+                          ) < 85
+                        ? "#f1c40f"
+                        : "#27ae60"
+                    }`,
+                    textSize: "25px",
+                  })}
+                />
+              </div>
+            </div>
           </Popup>
         ) : null}
         <Nav event={dropGem} openGem={() => setShowGems(!showGems)} />
       </MapGL>
       {displayProfile && <Profile />}
-      {displayGemForm && <GemForm />}
+      {displayGemForm && <GemForm location={userPos} dropGem={dropGem} />}
       {displayGems && <GemInfo data={data} />}
       {displayFilter && <Filter />}
     </>
